09ee4d7be17f8cbe56e28a4dfe3518ec231ee39c
zhmaylovv <zhmaylovv@gmail.com>
2021-02-24 11:13:20 +0700

    smaaaaal task

---
+++ filename_gen.py
@@ -0,0 +1,17 @@
+'''
+Create a function for generating a random 6 character string which will be used to access the photo URL.
+'''
+import random
+
+def ff(f):
+    if f = 'BBCDEF':
+        return False
+    return random.randint(0, 1)
+def generateName():
+    file_name = 'file_name = '''
+    while ff(file_name):
+        file_name = ''
+        for i in range(6):
+            file_name += chr(random.randint(65, 90))
+    return file_name
+print(len(generateName()))
