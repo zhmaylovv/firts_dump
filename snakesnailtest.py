e123430e0449c9c956c9b797aaeb459dc48ff178
zhmaylovv <zhmaylovv@gmail.com>
2021-03-04 13:34:27 +0700

    snake n*n

---
+++ snakesnailtest.py
@@ -0,0 +1,28 @@
+def snail(snail_map):
+    if len(snail_map[0]) == 0:
+        return []
+
+    n = len(snail_map)
+    x = 0
+    res = []
+
+    for i in range(n//2): #zad
+        for j in range(x, n-1-x):
+            res.append(snail_map[x][j])
+        for j in range(x, n-1-x):
+            res.append(snail_map[j][n-1-x])
+        for j in range(n-1-x, x, -1):
+            res.append(snail_map[n-1-x][j])
+        for j in range(n-1-x, x, -1):
+            res.append(snail_map[j][x])
+        x += 1
+    if n %2 !=0:
+        res.append(snail_map[n//2][n//2])
+
+    return res
+
+m = [  [1, 2, 3, 4, 5],
+        [14, 15, 16, 17, 6],
+        [13, 20, 19, 18, 7],
+        [12, 11, 10, 9, 8]]
+print(snail(m)) # не работает на не n*n массивах  )))
